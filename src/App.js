54eda7ff3bb2c22637bba24c0cf066cb69c665be
unknown <jagdishtiwari0582@gmail.com>
2024-11-29 16:56:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [todolist, setTodolist] = useState([]);
+
+ 
+
+  let SaveData = (event) => {
+    let toname = event.target.toname.value;
+    if (!todolist.includes(toname)) {
+      let finalTodo = [...todolist, toname];
+      setTodolist(finalTodo);
+    } else {
+      alert("ToDo name already Exists.....");
+    }
+    event.preventDefault();
+  };
+
+  let list = todolist.map((value, index) => {
+    return (
+      <TodoListItem key={index} value={value}  indexNumber={index}
+      todolist={todolist}
+      setTodolist={setTodolist}
+ />
+      
+    )
+  });
+    
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>ToDo List</h2>
+      <form onSubmit={SaveData}>
+        <input type="text" name="toname" />
+        <button>Save</button>
+      </form>
+
+      <div className="outerDiv">
+        <ul>{list}</ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function TodoListItem({ value , indexNumber , todolist , setTodolist}) {
+
+  let [status,setStatus]=useState(false);
+  let deleteRow=()=>{
+    let finalData=todolist.filter((v,i)=>i!=indexNumber)
+    setTodolist(finalData)
+    
+    
+  } 
+
+  let checkStatus=()=>{
+    setStatus(!status)
+  }
+  return (
+    <li className={(status)?'completetodo':''} onClick={checkStatus}>
+    {indexNumber+1}  {value} <span onClick={deleteRow}>&times;</span>
+    </li>
+  )
+}
